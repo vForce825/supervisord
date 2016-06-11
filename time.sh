489a652d6281ea6523c1261e426697366c479edc
柳啸虎 <liuxh3388@gmail.com>
2016-06-11 09:20:00 +0800

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-clear
 cat /proc/uptime| awk -F. '{run_days=$1 / 86400;run_hour=($1 % 86400)/3600;run_minute=($1 % 3600)/60;run_second=$1 % 60;printf("系统已运行：%d天%d时%d分%d秒",run_days,run_hour,run_minute,run_second)}'
